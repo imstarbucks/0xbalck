f0e50cb9cd16ff76e8797f4455df986fe0655249
Spencer <spencer.w@osbrjp.com>
2025-05-04 20:40:16 +0800

    update alpha-dashboard

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,9 +4,14 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim', 'desdic/telescope-rooter.nvim' },
   config = function()
     local telescope = require('telescope')
+
     telescope.setup({
       defaults = {
-        theme = "dropdown"
+      },
+      pickers = {
+        oldfiles = {
+          only_cwd = true -- optional: only show files from current working dir
+        }
       },
       extensions = {
         rooter = {
@@ -14,11 +19,13 @@ return {
         }
       },
     })
+
     telescope.load_extension('rooter')
   end,
   keys = {
     { "<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Find Files" },
     { "<leader>fg", function() require("telescope.builtin").live_grep() end,  desc = "Live Grep" },
+    { "<leader>fo", function() require("telescope.builtin").oldfiles() end,   desc = "Find Oldfiles" },
     { "<leader>l",  function() require("telescope.builtin").buffers() end,    desc = "List Buffers" },
   }
 }
